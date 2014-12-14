f13118f966b68fd7b471b51f764bdf6d39c49489
Brad Abrahams <email@bradabrahams.co.uk>
2014-12-14 16:47:24 +0000

    Beginnings of modules

--- assets/js/site/container.js
+++ assets/js/site/container.js
@@ -15,16 +15,51 @@ Maraschino.Container.Base = React.createClass({
         };
     },
 
+    componentDidMount: function() {
+        // set the number of columns in the grid, for the benefit of the CSS
+        document.getElementById('container').classList.add('columns-' + this.state.columns.length);
+    },
+
     render: function() {
+        var columns = this.state.columns.map(function(column) {
+            return (
+                <Maraschino.Container.Column modules={column} />
+            );
+        });
+
         return (
             <div className="row">
+                {columns}
             </div>
         );
     }
 });
 
 Maraschino.Container.Column = React.createClass({
-    render: function () {
-        return null;
+    render: function() {
+        var modules = this.props.modules.length === 0 ? (
+            <span>&nbsp;</span>
+        ) : this.props.modules.map(function(module) {
+            return (
+                <Maraschino.Container.Module module={module} />
+            );
+        });
+
+        return (
+            <div className="column">
+                {modules}
+            </div>
+        );
+    }
+});
+
+Maraschino.Container.Module = React.createClass({
+    render: function() {
+        var module = this.props.module;
+        return (
+            <div className="module">
+                <p>Module: {module}</p>
+            </div>
+        );
     }
 });
