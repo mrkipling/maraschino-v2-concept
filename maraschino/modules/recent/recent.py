7f2e309f952aa087a18ab4e4076938ed1a9f33b3
Brad Abrahams <email@bradabrahams.co.uk>
2014-12-22 13:31:31 +0000

    Actually return recent episodes

--- maraschino/modules/recent/recent.py
+++ maraschino/modules/recent/recent.py
@@ -9,11 +9,5 @@ module = Blueprint('recent', __name__,
 @module.route('/episodes/')
 def recent():
     xbmc = xbmc_api()
-
-    episodes = {
-        "episodes": [{
-            "name": "Test"
-        }]
-    }
-
+    episodes = xbmc.VideoLibrary.GetRecentlyAddedEpisodes(properties = ['title', 'season', 'episode', 'showtitle', 'playcount', 'thumbnail', 'tvshowid'])['result']['episodes']
     return json.dumps(episodes)
