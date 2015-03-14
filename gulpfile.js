f51cff5cb12b8aee7aeb06cc6e6a5803bd36ab4f
Brad Abrahams <email@bradabrahams.co.uk>
2015-03-14 12:24:39 +0000

    livereload and watchify

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,23 @@
 var gulp = require('gulp'),
     autoprefixer = require('gulp-autoprefixer'),
     browserify = require('browserify'),
+    buffer = require('gulp-buffer'),
     cache = require('gulp-cache'),
     concat = require('gulp-concat'),
+    gulpif = require('gulp-if'),
     imagemin = require('gulp-imagemin'),
     less = require('gulp-less'),
+    livereload = require('gulp-livereload'),
     minifycss = require('gulp-minify-css'),
     react = require('gulp-react'),
     rename = require('gulp-rename'),
     rimraf = require('gulp-rimraf'),
+    source = require('vinyl-source-stream'),
     sourcemaps = require('gulp-sourcemaps'),
     transform = require('vinyl-transform'),
     uglify = require('gulp-uglify'),
-    watch = require('gulp-watch');
+    watch = require('gulp-watch'),
+    watchify = require('watchify');
 
 var paths = {
     styles: {
@@ -56,23 +61,52 @@ gulp.task('styles:site', function() {
 
 /*--- scripts ---*/
 
+var watching = false;
+
 gulp.task('scripts:site', function() {
-    var browserified = transform(function(filename) {
-        return browserify({
-            entries: filename,
-            extensions: ['.jsx'],
-            debug: true
-        }).bundle();
+    browserified();
+});
+
+gulp.task('scripts:site:watch', function() {
+    watching = true;
+    browserified();
+});
+
+// Browserify site scripts
+
+function browserified() {
+    var b = browserify({
+        cache: {},
+        debug: true,
+        extensions: ['.jsx'],
+        fullPaths: true,
+        packageCache: {}
     });
 
-    return gulp.src(paths.scripts.site)
-        .pipe(browserified)
-        .pipe(sourcemaps.init({ loadMaps: true }))
-        .pipe(uglify())
-        .pipe(sourcemaps.write('./'))
-        .pipe(rename('site.js'))
-        .pipe(gulp.dest('./static/js'));
-});
+    if (watching) {
+        // if watch is enabled, wrap this bundle inside watchify
+        b = watchify(b);
+        b.on('update', function(){
+            bundle(b);
+        });
+    }
+
+    b.add(paths.scripts.site);
+    bundle(b);
+}
+
+// Browserify bundle and other plugins
+
+function bundle(b) {
+    b.bundle()
+        .pipe(source('site.js'))
+        .pipe(buffer())
+        //.pipe(sourcemaps.init({ loadMaps: true }))
+        //.pipe(uglify())
+        //.pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./static/js'))
+        .pipe(gulpif(watching, livereload()));
+}
 
 
 
@@ -118,14 +152,21 @@ function handleError(err) {
 
 /*--- watch ---*/
 
-var watching = false;
-
 gulp.task('watch', function () {
     watching = true;
     gulp.watch('./assets/less/lib/*', ['styles:lib']);
     gulp.watch('./assets/less/site/*', ['styles:site']);
     gulp.watch('./assets/js/site/*', ['scripts:site']);
     gulp.watch('./assets/images/*', ['default']);
+});
+
+gulp.task('watch', ['scripts:site:watch'], function() {
+    gulp.watch('./assets/less/lib/*', ['styles:lib']);
+    gulp.watch('./assets/less/site/*', ['styles:site']);
+    gulp.watch('./assets/images/*', ['default']);
+
+    // start live reload server
+    livereload.listen(35729);
 });
 
 
