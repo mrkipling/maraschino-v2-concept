37e1f5fc495ba0bce29a981ee11a397225743b53
Brad Abrahams <email@bradabrahams.co.uk>
2015-03-27 21:19:28 +0000

    Don't return thumbnails for recent episodes (yet)

--- maraschino/modules/recent/recent.py
+++ maraschino/modules/recent/recent.py
@@ -7,5 +7,5 @@ module = Blueprint('recent', __name__)
 @module.route('/episodes/')
 def recent():
     xbmc = xbmc_api()
-    episodes = xbmc.VideoLibrary.GetRecentlyAddedEpisodes(properties = ['title', 'season', 'episode', 'showtitle', 'playcount', 'thumbnail', 'tvshowid'])['result']['episodes']
+    episodes = xbmc.VideoLibrary.GetRecentlyAddedEpisodes(properties = ['title', 'season', 'episode', 'showtitle', 'playcount', 'tvshowid'])['result']['episodes']
     return json.dumps(episodes)
